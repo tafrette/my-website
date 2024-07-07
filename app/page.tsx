5c1eee82aae3438d0f187c63b20f63c31a4faaa7
Thomas Addisu Frette <thomas.frette@gmail.com>
2024-07-07 22:10:42 +0200

    change style

--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,28 @@ import React from "react";
 const UnderConstructionPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold mb-8 text-gray-800">
+      <h1 className="text-3xl font-bold mb-5 text-gray-800">
         Under Construction
       </h1>
       <div className="flex space-x-2">
         <div
-          className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"
+          className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"
           style={{ animationDelay: "0s" }}
         ></div>
         <div
-          className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"
+          className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"
           style={{ animationDelay: "0.2s" }}
         ></div>
         <div
-          className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"
+          className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"
           style={{ animationDelay: "0.4s" }}
         ></div>
         <div
-          className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"
+          className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"
           style={{ animationDelay: "0.6s" }}
         ></div>
         <div
-          className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"
+          className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"
           style={{ animationDelay: "0.8s" }}
         ></div>
       </div>
